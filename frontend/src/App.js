bf2b801c470044b202d05c1ec71bd316a7b6e4b5
yosef604 <yosef604@gmail.com>
2021-06-06 17:34:54 +0300

    header n footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import Footer from './components/Footer'
+import Header from './components/Header'
+import {Container} from 'react-bootstrap'
+
 const App = () => {
   return (
     <>
-      <h1>hello</h1>
+    <Header />
+    <Container>
+    <main className='py-3'>
+      <h1>hello  trftddydyd</h1>
+    </main>
+    </Container>
+    <Footer />
     </>
   )
 
