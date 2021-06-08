0e05f6db022d55d690b22784811fc04a364e57ff
yosef604 <yosef604@gmail.com>
2021-06-08 11:24:58 +0300

    adding controlers to the routes

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,29 +1,13 @@
 import express from 'express'
-import Product from '../models/ProductsModel.js'
-import asyncHandler from 'express-async-handler'
+import { getProducts, getProductsById } from '../controlers/productsControler.js'
 
 const router = express.Router()
 
-//@desc   Fetch all products
-//@route   GET /api/products
-//@daccess   public
-router.get('/', asyncHandler(async(req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
 
-//@desc   Fetch a single product
-//@route   GET /api/products/:id
-//@daccess   public
-router.get('/:id', asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
+router.route('/').get(getProducts)
 
-    if (product){
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not Found')
-    }
-}))
+router.route('/:id').get(getProductsById)
+
+
 
 export default router
