b26687130878535b6d16d482d2c03cd31385f4f9
yosef604 <yosef604@gmail.com>
2021-06-08 15:43:01 +0300

    costum authentication middleware

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { authUser } from '../controlers/usersControlers.js'
+import { authUser, getUserProfile } from '../controlers/usersControlers.js'
+import { protect } from '../middleare/AuthMiddlewarw.js'
 
 const router = express.Router()
 
 
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 
 
