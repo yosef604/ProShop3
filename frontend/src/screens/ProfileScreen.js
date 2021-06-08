db3429ecbf136d6198d43da4583a8e8d0b30c3d0
yosef604 <yosef604@gmail.com>
2021-06-09 01:19:41 +0300

    update user profile

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { Button, Col, Form, FormControl, FormGroup, FormLabel, Row } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-import { getUserDetails } from '../actions/usersActions'
+import { getUserDetails, updateUserProfile } from '../actions/usersActions'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
+import { USER_UPDATE_PROFILE_RESET } from '../constants/usersConstants'
 
 const ProfileScreen = ({location, history}) => {
     const [name, setName] = useState('')
@@ -20,26 +21,34 @@ const ProfileScreen = ({location, history}) => {
     const userLogin = useSelector(state => state.userLogin)
     const {userInfo} = userLogin
 
+    const userUpdateProfile = useSelector(state => state.userUpdateProfile)
+    const {success} = userUpdateProfile
+
 
     useEffect(() => {
         if(!userInfo) {
             history.push('/login')
         } else {
-            if(!user.name){
+            if(!user || !user.name || success){
+                dispatch({type: USER_UPDATE_PROFILE_RESET})
                 dispatch(getUserDetails('profile'))
             } else {
                 setName(user.name)
                 setEmail(user.email)
             }
         }
-    }, [history, userInfo, dispatch, user])
+    }, [history, userInfo, dispatch, user, success])
 
     const submitHandler = (e) => {
         e.preventDefault()
         if (password !== confirmPassword){
             setMessage('Passwords do not match!!')
         } else {
-            //dispatch
+            dispatch(updateUserProfile({
+                id: user._id,
+                name,
+                password
+            }))
         }
     }
     return (
@@ -48,6 +57,7 @@ const ProfileScreen = ({location, history}) => {
             <h2>User Profile</h2>
             {loading && <Loader />}
             {message && <Message variant='danger'>{message}</Message>}
+            {success && <Message variant='success'>Profile Updated</Message>}
             {error && <Message variant='danger'>{error}</Message>}
             <Form onSubmit={submitHandler}>
                 <FormGroup controlId='name'>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,12 @@ import {
     , productsListReducer 
 } from './reducers/productsReducers'
 import { cartReducer } from './reducers/cartReducer'
-import { userDetailsReducer, userLoginReducer, userRegisterReducer } from './reducers/usersReducers'
+import {
+     userDetailsReducer, 
+     userLoginReducer, 
+     userRegisterReducer, 
+     userUpdateProfileReducer 
+    } from './reducers/usersReducers'
 
 const reducer = combineReducers({
     productsList: productsListReducer,
@@ -14,7 +19,8 @@ const reducer = combineReducers({
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
-    userDetails: userDetailsReducer
+    userDetails: userDetailsReducer,
+    userUpdateProfile: userUpdateProfileReducer
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? 
