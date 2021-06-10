51b34890a25671e9d10907fcd3becf220e548288
yosef604 <yosef604@gmail.com>
2021-06-10 16:20:25 +0300

    reseting user info n urders at logout

--- frontend/src/reducers/ordersReducers.js
+++ frontend/src/reducers/ordersReducers.js
@@ -7,6 +7,7 @@ import {
     ORDER_DETAILS_SUCCESS,
     ORDER_MY_LIST_FAIL,
     ORDER_MY_LIST_REQUEST,
+    ORDER_MY_LIST_RESET,
     ORDER_MY_LIST_SUCCESS,
     ORDER_PAY_FAIL,
     ORDER_PAY_REQUEST,
@@ -63,6 +64,8 @@ export const myOrdersListReducer = (state = {orders: []}, action) => {
             return {loading: false, orders: action.payload}
         case ORDER_MY_LIST_FAIL:
             return {loading: false, error: action.payload}
+        case ORDER_MY_LIST_RESET:
+            return {orders: []}
         default:
             return state
     }
