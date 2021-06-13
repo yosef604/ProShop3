d75920836baa6c29fb5c5df218ea8a95d1d56464
yosef604 <yosef604@gmail.com>
2021-06-13 03:47:32 +0300

    multer configuration

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productsRouter from './routes/productsRoute.js'
 import usersRouter from './routes/usersRoutes.js'
 import ordersRouter from './routes/ordersRoute.js'
+import uploadRouter from './routes/uploadRoutes.js'
 import { errorHandler, notFound } from './middleare/errorMiddlewares.js'
 
 dotenv.config()
@@ -19,9 +21,13 @@ app.get('/', (req, res) => {
     res.send('API is runnig')
 })
 
+const __dirname = path.resolve()
+app.use('/upload', express.static(path.join(__dirname, '/upload')))
+
 app.use('/api/products', productsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/orders', ordersRouter)
+app.use('/api/upload', uploadRouter)
 
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
